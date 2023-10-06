190df18701037668faaf8faa06314bc92306d529
TkalychMaksim <tkalychmaksim@gmail.com>
2023-10-06 12:02:46 +0300

    reworked task 1

--- Lection 1/Task 1/main.py
+++ Lection 1/Task 1/main.py
@@ -1,3 +1,1 @@
-user_name_surname = input("Enter your name and surname separated by space: ")
-name, surname = user_name_surname.split()
-print(f"Your name: {name}\nSurname: {surname}")
+print("Name: Maksym \nSurname: Tkalych")
