cdb26ec2993179e6c1bc6833e24cf1a9bdd750bf
TkalychMaksim <tkalychmaksim@gmail.com>
2023-10-21 15:57:48 +0300

    reworked task 4

--- Lection 2/Task 4/main.py
+++ Lection 2/Task 4/main.py
@@ -1,7 +1,5 @@
 user_letters_list = input("Enter your string: ")
 vowels = 'aeiouy'
-user_vowels_list = []
 for letter in user_letters_list:
     if letter in vowels:
-        user_vowels_list.append(letter)
-print(f"Vowels: {' '.join(user_vowels_list)}")
+        print(letter, end=' ')
