d2988dc0fd4368cf85a2cb39206d929a941d64f7
TkalychMaksim <tkalychmaksim@gmail.com>
2023-10-21 12:18:58 +0300

    reworked task 2

--- Lection 2/Task 2/main.py
+++ Lection 2/Task 2/main.py
@@ -1,7 +1,10 @@
 user_list = input("Enter your elements separated by space: ").split()
-numbers_list = []
+number_counter = 0
+print("Numbers list: ")
 for element in user_list:
     if element.isdigit():
-        numbers_list.append(element)
-print(f"Count of numbers in your string: {len(numbers_list)}\n{', '.join(str(number)for number in numbers_list)}")
+        print(element)
+        number_counter += 1
+print("The count of numbers: " + str(number_counter))
+
 
