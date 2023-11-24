0f770e56252873ff7f2447d856a6ee5ddfcff269
TkalychMaksim <tkalychmaksim@gmail.com>
2023-11-25 00:01:40 +0200

    add main files to task 1

---
+++ Lection 5/app/database.py
@@ -0,0 +1,50 @@
+from app import db
+access_status = False
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(256))
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=False)
+    message = db.Column(db.String(256))
+    message_date = db.Column(db.DateTime)
+
+
+def register_user(username, password):
+    try:
+        user = Users(username=username, password=password)
+        db.session.add(user)
+        db.session.flush()
+        db.session.commit()
+    except Exception as error:
+        print(error)
+
+
+def login_user(username, password):
+    global access_status
+    users = Users.query.all()
+    for user in users:
+        if user.username == username and user.password == password:
+            access_status = True
+            break
+        else:
+            access_status = False
+    return access_status
+
+
+def add_message(username, message, message_date):
+    try:
+        new_message = Messages(username=username, message=message, message_date=message_date)
+        db.session.add(new_message)
+        db.session.flush()
+        db.session.commit()
+    except Exception as error:
+        print(error)
+
+
+
---
+++ Lection 5/app/routes.py
@@ -0,0 +1,49 @@
+from flask import render_template, request, redirect, make_response
+from app import app
+from app.database import register_user, login_user, add_message, Messages
+from datetime import datetime
+
+
+@app.route('/')
+def redirect_to_register():
+    return redirect('/register')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def user_register():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmed_password = request.form.get('confirmed_password')
+        if password == confirmed_password:
+            register_user(username, password)
+            return redirect('/send')
+        else:
+            return render_template('register.html', error='Error: Passwords do not match')
+    if request.method == "GET":
+        return render_template('register.html')
+
+
+@app.route('/send', methods=['GET', 'POST'])
+def user_login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        message = request.form.get('message')
+        if login_user(username, password):
+            add_message(username=username, message=message, message_date=datetime.utcnow())
+            return redirect('/all')
+        else:
+            return render_template('send.html', error='Error: Passwords do not match')
+    if request.method == 'GET':
+        return render_template('send.html')
+
+
+@app.route('/all')
+def show_messages():
+    messages = Messages.query.all()
+    data = ""
+    for message in messages:
+        data += "{}: {} [{}]<br>".format(message.username, message.message, message.message_date)
+    responce = make_response(data)
+    return responce
