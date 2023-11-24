9200371eaf888bc97a3aef614b999816b5bf83c8
TkalychMaksim <tkalychmaksim@gmail.com>
2023-11-25 00:10:08 +0200

    rework task 1

--- Lection 3/Task 1/main.py
+++ Lection 3/Task 1/main.py
@@ -1,19 +1,11 @@
 input_filename = input("Enter the filename with '.txt' end: ")
-symbols_vocabulary = {}
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 try:
-    with open(input_filename, 'r') as f:
-        for symbol in f.read():
-            if symbol in symbols_vocabulary:
-                symbols_vocabulary[symbol] += 1
-            else:
-                symbols_vocabulary[symbol] = 1
-except IOError as e:
+    with open(input_filename, 'r') as file:
+        text = file.read()
+        for symbol in set(text):
+            count = text.count(symbol)
+            print(f"{symbol}: {count}")
+except IOError as error:
     print("Error opening file")
-finally:
-    for symbol in symbols_vocabulary:
-        print(f"{symbol}: {symbols_vocabulary[symbol]}")
-
-
 
 
